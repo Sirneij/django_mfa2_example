2c89f3f5627fe26a51ce8d38819b9dfab8071c27
John Idogun <Sirneij@gmail.com>
2021-05-25 19:07:40 +0100

    Updated admin.py

--- mfa/admin.py
+++ mfa/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User_keys
 
-# Register your models here.
+
+class CustomUserAdmin(admin.ModelAdmin):
+    model = User_keys
+    list_display = ('username',)
+
+admin.site.register(User_keys, CustomUserAdmin)

141e6e4e15086d07cef7620ac32defbdc904448c
John Idogun <sirneij@gmail.com>
2021-05-26 10:08:12 +0100

    Added migrations

--- mfa/admin.py
+++ mfa/admin.py
@@ -4,4 +4,4 @@ from mfa.models import User_Keys
 
 @admin.register(User_Keys)
 class User_KeysAdmin(admin.ModelAdmin):
-    list_display = ('username',)
+    list_display = ('username', 'added_on', 'key_type', 'owned_by_enterprise',)
